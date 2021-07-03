ac75418000990ff722c42afdaf64e5d268dee6f4
daniel nwangwu <dannynwangwu@gamil.com>
2021-07-03 21:13:27 +0100

    secret model modification

--- users/models.py
+++ users/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.db.models.base import Model
+from django.db.models.deletion import SET_NULL
 from django.utils.translation import ugettext_lazy as _
 
 from .managers import CustomUserManager
@@ -24,3 +25,5 @@ class CustomUser(AbstractUser):
 
 class Secret(models.Model):
     secrete_key = models.IntegerField(blank=True, null=True)
+    sender = models.ForeignKey(to=CustomUser, null=True, on_delete=models.SET_NULL)
+    reciever = models.ForeignKey(to=CustomUser, null=True, on_delete=models.SET_NULL)
