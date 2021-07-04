8a4395567fb477675106edf141647b62d4e6ef42
dannicada <dannynwangwyu@gmail.com>
2021-07-04 07:28:06 +0100

    user urls views and srializers

--- users/views.py
+++ users/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+from .serializers import *
+from .models import Exchange
+from rest_framework import generics, serializers, status,permissions
 
 # Create your views here.
+
+
+class UserView(ModelViewSet):
+    serializer_class = UserSerializer
+    permission_classes = [permissions.AllowAny, ]
+
+    def create(self, request, *args, **kwargs):
+        """
+        create a new Exchange
+        """
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
