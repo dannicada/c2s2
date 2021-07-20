212fafa3c736a06d18ee3086c0e9c2ee2c31e3af
dannicada <dannynwangwyu@gmail.com>
2021-07-20 16:04:38 +0100

    public key generation

---
+++ users/signals.py
@@ -0,0 +1,25 @@
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+@receiver(post_save, sender=User, dispatch_uid="my_id")
+def generate_and_assign_publickey(sender, **kwargs):
+    instance, created = kwargs["instance"], kwargs["created"]
+    if created:
+        try:        
+            # generate 64 bit random number
+            user_public_key = random.getrandbits(64)
+
+            # assign random number to user
+            instance.publickey = user_public_key
+            instance.save()
+        except IntegrityError:
+            # handle integrity error by generating and assigning new random nuber
+            user_public_key = random.getrandbits(64)
+            instance.publickey = user_public_key
+            pass
+    
--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('users/all/', views.UserView.as_view({'get':'list'}), name='all-users'),
     path('users/<int:pk>', views.UserView.as_view({'get':'retrieve'}), name='retrive-user'),
-    # path('users/register/', views.UserView.as_view({'post':'create'}), name='register-new-user'),
+    path('users/register/', views.UserView.as_view({'post':'create'}), name='register-new-user'),
     # # path('team/delete/<int:pk>/',views.TeamView.as_view(get= 'destroy'), name='delete-team'),
     # path('team/update/<int:pk>/',views.UpdateTeamView.as_view(), name='update-team'),
     # path('team/add-member/<int:pk>/',views.AddMemberToTeamView.as_view(), name='update-team')
