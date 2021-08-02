fec7e3162cc19c0201ad44328a5be63a759d7ccb
dannicada <dannynwangwyu@gmail.com>
2021-08-02 07:48:14 +0100

    api url modification

--- C2S2/urls.py
+++ C2S2/urls.py
@@ -27,7 +27,7 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="ava API",
+      title="C2S2 API",
       default_version='v1',
       description="Test description",
       terms_of_service="https://www.google.com/policies/terms/",
